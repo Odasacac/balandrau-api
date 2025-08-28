8afe79c53a2ca5b21ae24f422a5e07153aa93578
CarlosCA <carloscasadoaguilera@gmail.com>
2025-08-28 15:29:20 +0200

    Primer commit. Proyecto creado

---
+++ src/main/java/CCASolutions/BalandrauAPI/BalandrauApiApplication.java
@@ -0,0 +1,13 @@
+package CCASolutions.BalandrauAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalandrauApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalandrauApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CCASolutions/BalandrauAPI/BalandrauApiApplicationTests.java
@@ -0,0 +1,13 @@
+package CCASolutions.BalandrauAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalandrauApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
